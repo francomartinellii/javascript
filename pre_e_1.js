29997a071c77cb03d07af864fef2d7274800e414
FrancoMartinelli <francomartinelli@hotmail.com>
2023-02-13 20:14:43 -0300

    2daPrentrega

--- pre_e_1.js
+++ pre_e_1.js
@@ -1,57 +1,60 @@
+
 //Alerta de bienvenida
 alert("Bienvenido a conversor de Divisas");
 
-//Funcion
+// Map y objeto
+const tasasDeCambio = new Map();
+tasasDeCambio.set("usd", { tasa: 380, simbolo: 'U$D', nombre: "Dolar Estadounidense" });
+tasasDeCambio.set("euro", { tasa: 410, simbolo: '€', nombre: "Euro" });
+tasasDeCambio.set("real", { tasa: 83, simbolo: 'R$', nombre: "Real Brasilero" });
+
+// Array monedas aceptadas
+const monedasAceptadas = ["usd", "euro", "real"];
+
+// Función  calcular 
 function cotizacion(monto, moneda) {
-
-    if (moneda == "usd") {
-        let cotizacion = monto * 380;
-        return cotizacion
+    if (tasasDeCambio.has(moneda)) {
+        let tasa = tasasDeCambio.get(moneda);
+        return `${monto * tasa.tasa}`;
+    } else {
+        return "Moneda no aceptada";
     }
-    else if (moneda == "euro") {
-        let cotizacion = monto * 410;
-        return cotizacion
-    }
-
-    else if (moneda == "real") {
-        let cotizacion = monto * 83;
-        return cotizacion
-    }
-
 }
-
 
 let cotizar = "si";
 
 while (cotizar === "si") {
     // Pedir moneda y monto a cotizar
     let moneda = prompt("Elige la moneda que quieres cotizar: usd, euro, real");
-    while (moneda != "usd" && moneda != "euro" && moneda != "real") {
+    while (!monedasAceptadas.includes(moneda)) {
         alert("Ingresa una moneda valida");
         moneda = prompt("Elige la moneda que quieres cotizar: usd, euro, real");
     }
-    
+
     let monto = prompt("Ingresa el monto:");
 
-    // Comprobar que el valor ingresado es un número
+    // Comprobar que sea un numero
     while (isNaN(monto)) {
         alert("Ingresa un número válido");
         monto = prompt("Ingresa el monto:");
     }
     monto = parseInt(monto);
-    
-    // Llama a la función cotizacion
-    let total = cotizacion(monto, moneda);
-    console.log("Nueva cotizacion");
-    console.log("Monto a cotizar:", monto);
-    console.log("Moneda:", moneda);
-    console.log("El total en pesos es:", total);
 
-    // Pregunta si quiere cotizar de nuevo
+    // Función cotizacion
+    let total = cotizacion(monto, moneda);
+
+    console.log("Nueva cotizacion");
+    console.log(`Monto a cotizar: ${tasasDeCambio.get(moneda).simbolo} ${monto} `);
+    console.log(`Moneda: ${tasasDeCambio.get(moneda).nombre} `);
+    console.log("El total en pesos es: $", total);
+    console.log("");
+
+    // Cotizar de nuevo
     cotizar = prompt("¿Quieres cotizar de nuevo? si/no");
     while (cotizar != "si" && cotizar != "no") {
-        alert("Ingresa una opcion valida");
+        alert("Ingresa una opción valida");
         cotizar = prompt("¿Quieres cotizar de nuevo? si/no");
     }
 }
+//Alerta despedida
 alert("Gracias por usar el conversor");
