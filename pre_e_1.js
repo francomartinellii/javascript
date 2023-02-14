1b8c4c9a945fd607f588692fa4200ccec20490f7
FrancoMartinelli <francomartinelli@hotmail.com>
2023-02-14 01:44:29 -0300

    add gbp

--- pre_e_1.js
+++ pre_e_1.js
@@ -1,62 +1,74 @@
 
-//Alerta de bienvenida
-alert("Bienvenido a conversor de Divisas");
+// Alerta de bienvenida
+alert("Bienvenido al Conversor de Divisas");
 
 // Map y objeto
-const tasasDeCambio = new Map();
-tasasDeCambio.set("usd", { tasa: 380, simbolo: 'U$D', nombre: "Dolar Estadounidense" });
-tasasDeCambio.set("euro", { tasa: 410, simbolo: '€', nombre: "Euro" });
-tasasDeCambio.set("real", { tasa: 83, simbolo: 'R$', nombre: "Real Brasilero" });
+const tasasDeCambio = new Map([
+  ["usd", { tasa: 380, simbolo: "$", nombre: "Dólar Estadounidense" }],
+  ["euro", { tasa: 410, simbolo: "€", nombre: "Euro" }],
+  ["real", { tasa: 83, simbolo: "R$", nombre: "Real Brasileño" }],
+  ["gbp" , { tasa: 450, simbolo: "£" , nombre:"Libra Esterlina"}],
+]);
 
 // Array monedas aceptadas
-const monedasAceptadas = ["usd", "euro", "real"];
+const monedasAceptadas = ["usd", "euro", "real" , "gbp"];
 
-// Función  calcular 
+// Función cotizacion
 function cotizacion(monto, moneda) {
-    if (tasasDeCambio.has(moneda)) {
-        let tasa = tasasDeCambio.get(moneda);
-        return `${monto * tasa.tasa}`;
-    } else {
-        return "Moneda no aceptada";
-    }
+  if (tasasDeCambio.has(moneda)) {
+    let tasa = tasasDeCambio.get(moneda);
+    let total = monto * tasa.tasa;
+    return {
+      simbolo: tasa.simbolo,
+      nombre: tasa.nombre,
+      total: total,
+    };
+  } else {
+    return null;
+  }
 }
 
-let cotizar = "si";
+// Bucle principal
+let cotizar = true;
 
-while (cotizar === "si") {
-    // Pedir moneda y monto a cotizar
-    let moneda = prompt("Elige la moneda que quieres cotizar: usd, euro, real");
-        moneda = moneda.toLowerCase ();
-    while (!monedasAceptadas.includes(moneda)) {
-        alert("Ingresa una moneda valida");
-        moneda = prompt("Elige la moneda que quieres cotizar: usd, euro, real");
-    }
+while (cotizar) {
+  // Pedir moneda y monto a cotizar
+  let moneda = prompt("Elige la moneda que quieres cotizar: usd, euro, real, gbp").toLowerCase();
+  while (!monedasAceptadas.includes(moneda)) {
+    alert("Moneda no válida");
+    moneda = prompt("Elige la moneda que quieres cotizar: usd, euro, real, gbp").toLowerCase();
+  }
 
-    let monto = prompt("Ingresa el monto:");
+  let monto = prompt("Ingresa el monto:");
+  while (isNaN(monto) || (monto) <= 0) {
+    alert("Monto no válido el monto debe ser positivo");
+    monto = prompt("Ingresa el monto:");
+  }
+  monto = parseInt(monto);
 
-    // Comprobar que sea un numero
-    while (isNaN(monto)) {
-        alert("Ingresa un número válido");
-        monto = prompt("Ingresa el monto:");
-    }
-    monto = parseInt(monto);
+  // Calcular cotización
+  let resultado = cotizacion(monto, moneda);
 
-    // Función cotizacion
-    let total = cotizacion(monto, moneda);
-
-    console.log("Nueva cotizacion");
-    console.log(`Monto a cotizar: ${tasasDeCambio.get(moneda).simbolo} ${monto} `);
-    console.log(`Moneda: ${tasasDeCambio.get(moneda).nombre} `);
-    console.log("El total en pesos es: $", total);
+  if (resultado) {
+    // Mostrar resultado
+    console.log("Nueva cotización:");
+    console.log(`Monto a cotizar: ${resultado.simbolo} ${monto}`);
+    console.log(`Moneda: ${resultado.nombre}`);
+    console.log(`Total en pesos: $${resultado.total}`);
     console.log("");
 
-    // Cotizar de nuevo
-    cotizar = prompt("¿Quieres cotizar de nuevo? si/no");
-    while (cotizar != "si" && cotizar != "no") {
-        alert("Ingresa una opción valida");
-        cotizar = prompt("¿Quieres cotizar de nuevo? si/no");
-        cotizar = cotizar.toLowerCase ();
+    // Preguntar si se desea cotizar de nuevo
+    let respuesta = prompt("¿Quieres cotizar de nuevo? si/no").toLowerCase();
+    while (respuesta !== "si" && respuesta !== "no") {
+      alert("Respuesta no válida");
+      respuesta = prompt("¿Quieres cotizar de nuevo? si/no").toLowerCase();
     }
+    cotizar = respuesta === "si";
+  } else {
+    alert("Ha ocurrido un error. Inténtalo de nuevo.");
+  }
 }
-//Alerta despedida
-alert("Gracias por usar el conversor");
+
+// Alerta de despedida
+alert("Gracias por usar el Conversor de Divisas");
+
